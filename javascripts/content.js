cc884522b4ca565f0fe41b9bc89f74aa8cc5ca30
Scott Munro <scottnmunro@gmail.com>
2016-05-24 19:00:21 -0500

    chore: updating personal site to include current work and ogre jrpg

--- javascripts/content.js
+++ javascripts/content.js
@@ -2,3 +2,6 @@ var app = angular.module('ngHomepageApp', []);
 app.controller("ProjectController", function($scope){
 	$scope.projects = projectData;
 });
+app.controller("CurrentWorkController", function($scope){
+	$scope.current_works = currentWorkData;
+});
