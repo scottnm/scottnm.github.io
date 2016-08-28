5b889cb75a0eadf0cdeadffe7e29737c720b2f26
Scott Munro <scottnmunro@gmail.com>
2016-08-27 22:53:26 -0500

    updated to have two separate project lists

--- javascripts/content.js
+++ javascripts/content.js
@@ -1,6 +1,9 @@
 var app = angular.module('ngHomepageApp', []);
-app.controller("ProjectController", function($scope){
-	$scope.projects = projectData;
+app.controller("TechnicalProjectController", function($scope){
+	$scope.tech_projects = tech_projectData;
+});
+app.controller("CreativeProjectController", function($scope){
+	$scope.creative_projects = creative_projectData;
 });
 app.controller("CurrentWorkController", function($scope){
 	$scope.current_works = currentWorkData;
