d48387c9284646230744e61689ce95f31911c88a
Scott Munro <scottnmunro@gmail.com>
2017-12-09 21:10:18 -0800

    mocking out individual tab panes

--- test.js
+++ test.js
@@ -3,5 +3,11 @@ window.onload =
     {
         var welcome_message="Vivamus ut venenatis magna, eu pharetra enim. Sed consectetur finibus ex, at luctus velit tincidunt nec. Aenean sed diam ligula. Cras massa justo, porta at nulla ut, bibendum consequat sem. Donec elementum cursus efficitur. Phasellus at convallis sem, quis rutrum tellus. Quisque quis semper ligula. Sed eros neque, imperdiet et ante sit amet, tristique pulvinar eros. Mauris posuere quis nunc consectetur fringilla. Ut interdum dui sit amet ligula tincidunt sodales. Curabitur tincidunt nulla sit amet metus pretium, nec scelerisque ipsum efficitur. Phasellus nec enim non est fermentum venenatis sed sed turpis. Suspendisse non nisi nulla.";
         document.getElementById("welcome-text").innerText = welcome_message;
-        document.getElementById("projects-test-text").innerText = welcome_message + welcome_message + welcome_message + welcome_message;
+        var project_elements = document.getElementsByClassName("project");
+        console.log(project_elements);
+        for (var i = 0; i < project_elements.length; i++)
+        {
+            project_elements[i].innerText = welcome_message;
+        }
+        
     };
