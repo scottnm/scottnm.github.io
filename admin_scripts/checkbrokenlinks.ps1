d500dc2ae612726c4f2bf0521faf6f51a3457db2
Scott Munro <scottnmunro@gmail.com>
2025-12-28 20:27:58 -0800

    make admin scripts not bloat git status

--- admin_scripts/checkbrokenlinks.ps1
+++ admin_scripts/checkbrokenlinks.ps1
@@ -7,13 +7,29 @@ if (! (Get-Command broken-links)) {
     throw "broken-links not installed! Install with ``cargo install broken-links``"
 }
 
-$args = @(
-    $StartUrl
-)
-if ($SkipUrls) {
-    $skipUrlArg = $SkipUrls -join ","
-    $args += "-s"
-    $args += $skipUrlArg
-}
+$originalDir = Get-Location
+try {
+    $ReportDir = (Join-Path $PSScriptRoot ".." "broken-links-output")
+    Remove-Item -Recurse -Force $ReportDir -ErrorAction SilentlyContinue | Out-Null
+    New-Item -ItemType Directory -Force $ReportDir | Out-Null
+    cd $ReportDir
 
-broken-links @args
+    $args = @(
+        $StartUrl
+    )
+    if ($SkipUrls) {
+        $skipUrlArg = $SkipUrls -join ","
+        $args += "-s"
+        $args += $skipUrlArg
+    }
+
+    broken-links @args
+
+    $reports = Get-ChildItem "./"
+    if ($reports) {
+        $reports | %{ Write-Host -foregroundcolor Yellow "Report @ $_"}
+    }
+}
+finally {
+    cd $originalDir
+}
